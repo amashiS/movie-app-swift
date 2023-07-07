b0655b6f01ac4dfb3719a293f08b08e5c958669a
“amashiS” <amashisilva@Amashis-Macbook-Air.local>
2023-07-07 10:26:08 +0530

    initial commit

---
+++ Movie App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movie App
+//
+//  Created by Amashi Silva on 2023-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie App
+//
+//  Created by Amashi Silva on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
